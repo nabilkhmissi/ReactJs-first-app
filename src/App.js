5e0ff9a55494a807b3e01805ca85098aacfad8e7
nabil_khmissi <bilakhmissi@gmail.com>
2023-09-01 23:49:24 +0100

    add loading

--- src/App.js
+++ src/App.js
@@ -2,25 +2,35 @@ import { useState } from 'react';
 import './App.css';
 import Posts from './components/posts/posts';
 import { data } from "./components/data"
+import Loading from './components/loading/loading';
 
 export function App() {
   const [posts, setPosts] = useState(data)
-
+  const [loading, setLoading] = useState(true)
 
   function doSearch(e) {
     const search = e.target.value
     let result = data.filter(item => item.title.toLocaleLowerCase().includes(search.toLocaleLowerCase()));
     setPosts(result);
   }
+  setTimeout(() => {
+    setLoading(false)
+  }, 1000);
 
-  return (
-    <>
+
+  let rendering = <Loading />
+  if (!loading) {
+    rendering = (<>
       <div className='navbar'>
+        <h2>Newsy</h2>
         <input className='search-bar' placeholder='search...' onKeyUp={doSearch} />
+        <div></div>
       </div>
       <Posts posts={posts} />
-    </>
-  );
+    </>)
+  }
+
+  return <>{rendering}</>;
 }
 
 export default App;
