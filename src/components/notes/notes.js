430f7cd87ab58f1356e270a76936cb832fc47c01
nabil_khmissi <bilakhmissi@gmail.com>
2023-09-11 23:06:47 +0100

    useRef

--- src/components/notes/notes.js
+++ src/components/notes/notes.js
@@ -1,4 +1,4 @@
-import { useContext } from 'react';
+import { useContext, useRef, useState } from 'react';
 import { NotesContext } from '../context/notesContext';
 import Note from '../note/note';
 
@@ -7,6 +7,9 @@ import Note from '../note/note';
 export default function Notes() {
 
     const notes = useContext(NotesContext);
+    const [timerStarted, setTimerStarted] = useState(false)
+
+    let timerRef = useRef(0);
 
     function handleNoteDone(id) {
         alert(id)
@@ -14,9 +17,24 @@ export default function Notes() {
         n.done = !n.done;
     }
 
+    function handleTimerStart() {
+        setTimerStarted(true)
+        timerRef.current = setTimeout(() => {
+            setTimerStarted(false);
+            alert("3 seconds ecoulés")
+        }, 3000);
+    }
+
+    function handleTimerStop() {
+        setTimerStarted(false);
+        timerRef.current = clearTimeout(timerRef.current)
+    }
+
     return (
         <NotesContext.Provider>
             <div className="notes">
+                <button onClick={handleTimerStart}>{timerStarted ? "timer started ..." : "Start Timer"}</button>
+                <button onClick={handleTimerStop}>Stop Timer</button>
                 {notes.map(n => <Note key={n.id} note={n} changeNoteDone={handleNoteDone} />)}
             </div>
         </NotesContext.Provider>
