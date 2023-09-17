0f1b776c6eb08c0bec0fcc081b4a55dd1d930a77
nabil_khmissi <bilakhmissi@gmail.com>
2023-09-17 20:56:16 +0100

    add clear filter

--- src/components/pizza/pizzas-menu.js
+++ src/components/pizza/pizzas-menu.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 import { useEffect, useState } from "react";
 import Pizza from "./pizza";
-import { Link, NavLink, useSearchParams } from "react-router-dom";
+import { useSearchParams } from "react-router-dom";
 
 export default function PizzasMenu() {
 
@@ -11,7 +11,7 @@ export default function PizzasMenu() {
         fetchData();
     }, []);
 
-    const [searchParams, setSerachParams] = useSearchParams();
+    const [searchParams, setSerachParams] = useSearchParams({});
 
     function fetchData() {
         axios
@@ -36,14 +36,15 @@ export default function PizzasMenu() {
             <h1 style={{ textAlign: "center", padding: "1rem 0" }}>Our Pizzas</h1>
             <div className="pizza-filter" style={{ display: "flex", justifyContent: "center", gap: "1rem" }}>
 
-                <button onClick={() => setSerachParams({ ingredient: "tomato" })}>Tomatoes</button>
-                <button onClick={() => setSerachParams({ ingredient: "mozarella" })}>Mozarella</button>
-                <button onClick={() => setSerachParams({ ingredient: "cheese" })}>Cheese</button>
+                <button onClick={() => setSerachParams({ ingredient: "tomato" })} className={filter === "tomato" ? 'selected' : ''}  >Tomatoes</button>
+                <button onClick={() => setSerachParams({ ingredient: "mozarella" })} className={filter === "mozarella" ? 'selected' : ''} >Mozarella</button>
+                <button onClick={() => setSerachParams({ ingredient: "cheese" })} className={filter === "cheese" ? 'selected' : ''}>Cheese</button>
+                {filter && <button onClick={() => setSerachParams({})}>Clear</button>}
 
-                <NavLink to="?ingredient=tomato" >Tomato</NavLink>
+                {/*           <NavLink to="?ingredient=tomato" >Tomato</NavLink>
                 <NavLink to="?ingredient=mozarella" >Mozarella</NavLink>
-                <NavLink to=".">Clear filter</NavLink>
-            </div>
+                <NavLink to=".">Clear filter</NavLink> */}
+            </div >
             {
                 list.length !== 0 ? (
                     <ul className="pizza-wrapper container" >
